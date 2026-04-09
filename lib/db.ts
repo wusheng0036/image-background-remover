3ab7665affe121c65a0823b9e2fb1c480fc1eaf2
wusheng0036 <wusheng0036@users.noreply.github.com>
2026-04-09 18:12:59 +0800

    fix: remove cloudflare types error

--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,7 @@
-export async function initDB(){} export async function addCredits(){} export async function createOrder(){} export async function getUserByEmail(){return null} export async function createUser(){} export async function updateOrderStatus(){} export async function getUserCredits(){return 100}
+export async function initDB() {}
+export async function addCredits() {}
+export async function createOrder() {}
+export async function getUserByEmail() { return null; }
+export async function createUser() {}
+export async function updateOrderStatus() {}
+export async function getUserCredits() { return 999; }
